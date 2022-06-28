08d32cfb7c98f493cef5d85ea5d336c6301780f1
Yair <yairlevyy5@gmail.com>
2022-06-28 21:45:00 +0200

    First commit

---
+++ Week2/Day3/Exercises/DailyChallenge/script.js
@@ -0,0 +1,37 @@
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+
+
+
+// let asterix = "*"; 
+
+// for (let number = 0; number <= 6; number++) {
+// 	console.log(asterix)
+// 	asterix+= "*"
+
+// }
+
+
+
+
+// let asterix = "*"; 
+
+// for (let number = 0; number <= 2; number++) {
+// 	console.log(asterix)
+// 	asterix+= "*";
+// 	for (let numbertwo = 2; numbertwo <= 4; numbertwo++)
+// 	    console.log(asterix)
+//         asterix += "*";
+//   }
+
+
+
+
