cd006b3c83339cd22e8d6230ac402de3cadd9fb3
Yair <yairlevyy5@gmail.com>
2022-07-03 16:35:29 +0200

    First commit

---
+++ Week3/Day1/Exercises/ExercisesXP/script1.js
@@ -0,0 +1,46 @@
+
+// 🌟 Exercise 1 : Users
+// Instructions
+// <div id="container">Users:</div>
+// <ul class="list">
+//     <li>John</li>
+//     <li>Pete</li>
+// </ul>
+// <ul class="list">
+//     <li>David</li>
+//     <li>Sarah</li>
+//     <li>Dan</li>
+// </ul>
+
+
+// Add the code above, to your HTML file
+
+// Using Javascript:
+// Retrieve the div and console.log it
+// Change the name “Pete” to “Richard”.
+// Change each first name of the two <ul>'s to your name.
+// Delete the <li> that contains the text node “Sarah”.
+
+// Bonus - Using Javascript:
+// Add a class called student_list to both of the <ul>'s.
+// Add the classes university and attendance to the first <ul>.
+
+
+
+// let div = document.getElementById('container');
+// console.log(div)
+
+// let listOne = document.querySelector('ul.list')
+// let pete = listOne.lastElementChild;
+// pete.innerText="Richard"
+
+
+// let firstList = listOne.firstElementChild;
+// firstList.innerText="Yair"
+
+// let listTwo = document.querySelectorAll('.list')[1]
+// let listTwoName = listTwo.firstElementChild;
+// listTwoName.innerText="Yair"
+// console.log(listTwo)
+
+// listTwo.removeChild(listTwo.children[1]);
---
+++ Week3/Day1/Exercises/ExercisesXP/script2.js
@@ -0,0 +1,36 @@
+// 🌟 Exercise 2 : Users And Style
+// Instructions
+// <div>Users:</div>
+// <ul>
+//     <li>John</li>
+//     <li>Pete</li>
+// </ul>
+
+
+// Add the code above, to your HTML file
+
+// Add a “light blue” background color and some padding to the <div>.
+
+// Do not display the <li> that contains the text node “John”.
+
+// Add a border to the <li> that contains the text node “Pete”.
+
+// Change the font size of the whole body.
+
+// Bonus: If the background color of the div is “light blue”, alert “Hello x and y” (x and y are the users in the div).
+
+
+// let divUsers = document.getElementsByTagName('div');
+
+// divUsers.style.backgroundColor='lightblue';
+// divUsers.style.padding = '5px';
+
+// let ul = document.getElementsByTagName('ul');
+// let john = ul.firstElementChild;
+// let pete = ul.lastElementChild;
+
+// john.style.display = 'none';
+// pete.style.border = '2px solid black';
+
+
+
