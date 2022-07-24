01f4c78577278e13364eb3d3b9afb0d673156a66
Yair Levy <yairlevyy5@gmail.com>
2022-07-24 15:23:30 +0200

    First commit

---
+++ Week6/ExercisesXP.sql
@@ -0,0 +1,31 @@
+-- INSERT INTO items(product_name, product_price)
+-- VALUES 
+--         ('Small Desk', 100),
+--         ('Large desk', 300),
+--         ('Fan', 80);
+
+-- CREATE TABLE customers(
+-- customer_id SERIAL PRIMARY KEY,
+-- first_name VARCHAR (25) NOT NULL,
+-- last_name VARCHAR (25) NOT NULL 
+-- )
+
+-- INSERT INTO customers(first_name, last_name)
+-- VALUES 
+--         ('Greg', 'Jones'),
+--         ('Sandra', 'Jones'),
+--         ('Scott', 'Scott'),
+--         ('Trevor', 'Green'),
+--         ('Melanie', 'Johnson');
+
+-- SELECT * FROM items
+
+-- SELECT * FROM items WHERE product_price > 80
+
+-- SELECT * FROM items WHERE product_price <= 300;
+
+-- SELECT * FROM customers WHERE last_name = 'Smith';
+
+-- SELECT * FROM customers WHERE last_name = 'Jones';
+
+-- SELECT * FROM customers WHERE last_name != 'Scott';
