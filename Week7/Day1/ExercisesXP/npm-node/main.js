85b59247407cd51dd7a5dc5baea1c755c75e7c4a
Yair Levy <yairlevyy5@gmail.com>
2022-07-31 12:59:59 +0200

    First commit

---
+++ Week7/Day1/ExercisesXP/npm-node/main.js
@@ -0,0 +1,14 @@
+// let a = 5;
+// let b = 10;
+
+// console.log(`Hello you are ${a+b}`)
+
+// console.log("Hi, thanks for running me with npm start!")
+
+let aa = 5;
+let bb = 10;
+let x;
+let c = 6;
+x = aa + bb;
+
+console.log(x+c);
